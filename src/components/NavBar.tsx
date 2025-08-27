3902b0aedca2be9cc429b20cf5b9e1c8b4107a04
Ezio Borges <eziopborges@gmail.com>
2025-08-27 14:16:36 -0300

    refatoração: ajusta navegação e remove links desnecessários no componente NavBar; adiciona configuração de reescrita no vercel.json

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,32 @@
 import Cookies from 'js-cookie';
 import { Image, Nav } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import type { userPropsType } from '../types/UserType';
 
 const NavBar = ({ user }: userPropsType) => {
-	console.log('navbar user ===> ', user);
+	const navigate = useNavigate();
 
 	const handleLogout = () => {
 		if (Cookies.get('mock_jwt')) {
 			Cookies.remove('mock_jwt');
 		}
+
+		navigate('/');
 	};
 
 	return (
 		<Nav
 			accessKey="/dashboard"
-			onSelect={selectedKey => alert(`selected ${selectedKey}`)}
 			className="d-flex justify-content-between align-items-center"
 		>
 			<Nav.Item className="w-25">
-				<Nav.Link href="/dashboard">
+				<Nav.Link as={Link} to="/dashboard">
 					<Image src={logo} style={{ maxWidth: '30vw' }} />
 				</Nav.Link>
 			</Nav.Item>
 			<Nav.Item className="w-25 d-flex align-items-center justify-content-center">
-				<Nav.Link href="/test" className="text-dark ">
+				<Nav.Link className="text-dark ">
 					<h5 className="text-center">
 						Seja bem-vindo, {user?.firstName}!
 					</h5>
@@ -32,7 +34,6 @@ const NavBar = ({ user }: userPropsType) => {
 			</Nav.Item>
 			<Nav.Item className="w-25 d-flex justify-content-center">
 				<Nav.Link
-					href="/"
 					className="text-danger fw-bolder logout-action"
 					onClick={handleLogout}
 				>

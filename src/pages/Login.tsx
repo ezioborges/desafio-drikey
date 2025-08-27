4afaa550120aa899ba9f672121df5ac54905f6ab
Ezio Borges <eziopborges@gmail.com>
2025-08-26 23:27:46 -0300

    refatoração: ajusta layout e responsividade da tela de Login; atualiza estilos das imagens decorativas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie';
 import { useState, type FormEvent } from 'react';
-import { Alert, Button, Form, Image, Row } from 'react-bootstrap';
+import { Alert, Button, Col, Form, Image, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { loginValidation } from '../Validations/loginValidation';
 import blueCircle from '../assets/decorative-blue-circle.svg';
@@ -42,75 +42,96 @@ function Login() {
 	};
 
 	return (
-		<div
-			className="container-fluid d-flex flex-column align-items-center justify-content-center"
-			style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
-		>
-			<Image src={greenDiamond} className="decorative-green-diamond" />
+		<>
+			<Row
+				className="d-flex justify-content-center align-items-center"
+				style={{ height: '100vh', width: '100vw' }}
+			>
+				<Image
+					src={yellowCircle}
+					className="decorative-yellow-circle"
+				/>
 
-			<Image src={yellowCircle} className="decorative-yellow-circle" />
+				<Image
+					src={greenDiamond}
+					className="decorative-green-diamond"
+				/>
 
-			<Image src={blueCircle} className="decorative-blue-circle" />
+				<Image src={blueCircle} className="decorative-blue-circle" />
 
-			<Image src={whiteCircle} className="decorative-white-circle-left" />
+				<Image
+					src={whiteCircle}
+					className="decorative-white-circle-left"
+				/>
 
-			<Image
-				src={whiteCircle}
-				className="decorative-white-circle-right"
-			/>
+				<Image
+					src={whiteCircle}
+					className="decorative-white-circle-right"
+				/>
 
-			<Row className="w-100 h-100 d-flex justify-content-center align-items-center">
-				<Form
-					onSubmit={handleLogin}
-					className="d-flex flex-column justify-content-center align-items-center rounded-3 shadow-sm bg-white h-75 w-50"
+				<Col
+					xs={10} // Ocupa 10 de 12 colunas em telas extra pequenas (celular)
+					sm={8} // Ocupa 8 de 12 colunas em telas pequenas
+					md={6} // Ocupa 6 de 12 colunas em telas médias (tablet)
+					lg={6} // Ocupa 6 de 12 colunas em telas grandes (desktop)
+					className="d-flex justify-content-center align-items-center"
 				>
-					{error && <Alert variant="danger">{error}</Alert>}
-					<Image className="mb-4" src={drikeyLogo} />
-					<Form.Group
-						className="mb-4 w-75"
-						controlId="formLoginEmail"
+					<Form
+						onSubmit={handleLogin}
+						className="d-flex flex-column align-items-center justify-content-center rounded shadow-sm bg-white p-5 w-100"
 					>
-						<Form.Label className="d-flex justify-content-start">
-							Email
-						</Form.Label>
-						<Form.Control
-							type="email"
-							value={email}
-							onChange={({ target }) => setEmail(target.value)}
-							placeholder="Email"
-						/>
-					</Form.Group>
+						{error && <Alert variant="danger">{error}</Alert>}
+						<Image className="mb-4 drikey-logo" src={drikeyLogo} />
+						<Form.Group
+							className="mb-4 w-100"
+							controlId="formLoginEmail"
+						>
+							<Form.Label className="d-flex justify-content-start">
+								Email
+							</Form.Label>
+							<Form.Control
+								type="email"
+								value={email}
+								onChange={({ target }) =>
+									setEmail(target.value)
+								}
+								placeholder="Email"
+							/>
+						</Form.Group>
 
-					<Form.Group
-						className="mb-4 w-75"
-						controlId="formLoginPassword"
-					>
-						<Form.Label className="d-flex justify-content-start">
-							Password
-						</Form.Label>
-						<Form.Control
-							type="password"
-							value={password}
-							onChange={({ target }) => setPassword(target.value)}
-							placeholder="Senha"
-						/>
-					</Form.Group>
-					<Form.Group
-						className="mb-4 d-flex w-75"
-						controlId="formLoginCheckbox"
-					>
-						<Form.Check type="checkbox" label="Lembre-me" />
-					</Form.Group>
-					<Button
-						variant="outline-danger"
-						type="submit"
-						className="w-75"
-					>
-						Entrar
-					</Button>
-				</Form>
+						<Form.Group
+							className="mb-4 w-100"
+							controlId="formLoginPassword"
+						>
+							<Form.Label className="d-flex justify-content-start">
+								Password
+							</Form.Label>
+							<Form.Control
+								type="password"
+								value={password}
+								onChange={({ target }) =>
+									setPassword(target.value)
+								}
+								placeholder="Senha"
+							/>
+						</Form.Group>
+						<Form.Group
+							className="mb-4 d-flex"
+							controlId="formLoginCheckbox"
+						>
+							<Form.Check type="checkbox" label="Lembre-me" />
+						</Form.Group>
+						<Button
+							variant="outline-danger"
+							type="submit"
+							className="w-100"
+						>
+							Entrar
+						</Button>
+					</Form>
+				</Col>
 			</Row>
-		</div>
+		</>
 	);
 }
 
